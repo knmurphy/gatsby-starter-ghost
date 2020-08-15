62e4a5656e9a236ca8549487a9aa3a8b60dad1e1
Kevin N. Murphy <knmurphy@users.noreply.github.com>
2020-08-15 16:44:35 -0700

    update to project specific info

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
-    siteUrl: `https://shs-editor.herokuapp.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://www.safehairstylist.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Safe Hair Stylist - Salon Chemical Safety Education Experts`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Internationally recognized as THE experts in Salon Chemical Safety Education. We literally wrote the book(s) on it.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `SHS`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
